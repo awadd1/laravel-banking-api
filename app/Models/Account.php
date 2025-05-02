374b792e95ee0fcc1f76384d48c8bd69614b2499
Awadd <muhammedawadd1@gmail.com>
2025-05-02 19:53:31 +0300

    Initial commit - Created models, migrations, and DB connection

---
+++ app/Models/Account.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Account extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+}
---
+++ app/Models/Transfer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Transfer extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $guarded = [];
+
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function senderAccount()
+    {
+        return $this->belongsTo(Account::class, 'sender_account_id');
+    }
+
+    public function recipient()
+    {
+        return $this->belongsTo(User::class, 'recipient_id');
+    }
+
+    public function recipientAccount()
+    {
+        return $this->belongsTo(Account::class, 'recipient_account_id');
+    }
+
+  
+}
