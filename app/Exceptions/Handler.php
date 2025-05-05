367f1f03c67df063b0a2203860ed55032d06d1c4
Awadd <muhammedawadd1@gmail.com>
2025-05-05 18:55:54 +0300

    Implement user login/logout with exception handling and unified API responses

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,22 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponseTrait;
+use Illuminate\Database\QueryException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Database\UniqueConstraintViolationException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\Response;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponseTrait;
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -24,7 +35,97 @@ class Handler extends ExceptionHandler
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            //
+            
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+
+        if ($request->expectsJson() || Str::contains($request->path(), 'api')) {
+            Log::error($e);
+
+            if ($e instanceof AuthenticationException) {
+                $statusCode = Response::HTTP_UNAUTHORIZED;
+                return $this->apiResponse([
+                    'message' => "Unauthenticated or Token expired, please try to login again",
+                    'success' => false,
+                    'exception' => $e,
+                    'error_code' => $statusCode,
+                ], $statusCode);
+            }
+
+           if ($e instanceof NotFoundHttpException) {
+                return $this->apiResponse([
+                    'message' => $e->getMessage(),
+                    'success' => false,
+                    'exception' => $e,
+                    'error_code' => $e->getStatusCode(),
+                ], $e->getStatusCode());
+            }
+
+            if ($e instanceof ValidationException) {
+                $statusCode = Response::HTTP_UNPROCESSABLE_ENTITY;
+                return $this->apiResponse([
+                    'message' => "Validation failed",
+                    'success' => false,
+                    'exception' => $e,
+                    'error_code' => $statusCode,
+                    'errors' => $e->errors(),
+                ], $statusCode);
+            }
+
+            if ($e instanceof ModelNotFoundException) {
+                $statusCode = Response::HTTP_NOT_FOUND;
+                return $this->apiResponse([
+                    'message' => "Resource could not be found",
+                    'success' => false,
+                    'exception' => $e,
+                    'error_code' => $statusCode,
+                ], $statusCode);
+            }
+
+            if ($e instanceof UniqueConstraintViolationException) {
+                $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+                return $this->apiResponse([
+                    'message' => "Duplicate entry found",
+                    'success' => false,
+                    'exception' => $e,
+                    'error_code' => $statusCode,
+                ]);
+            }
+
+            if ($e instanceof QueryException) {
+                $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+                return $this->apiResponse([
+                    'message' => "Could not execute query",
+                    'success' => false,
+                    'exception' => $e,
+                    'error_code' => $statusCode,
+                ]);
+            }
+
+            if ($e instanceof \Exception) {
+                $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+                return $this->apiResponse([
+                    'message' => "We could not handle your request, please try again later",
+                    'success' => false,
+                    'exception' => $e,
+                    'error_code' => $statusCode,
+                ]);
+            }
+
+            if ($e instanceof \Error) {
+                $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+                return $this->apiResponse([
+                    'message' => "We could not handle your request, please try again later",
+                    'success' => false,
+                    'exception' => $e,
+                    'error_code' => $statusCode,
+                ]);
+            }
+        }
+
+        return parent::render($request, $e);
     }
 }
